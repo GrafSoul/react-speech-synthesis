8371d7ec22102b4c7b87acb7cbbd55a1dc854e8e
GrafSoul <grafsoul@gmail.com>
2020-09-14 19:00:17 +0500

    feat: Added the docs  folder.

---
+++ docs/precache-manifest.98ec0b4eb4fb2ee05be2ea9e0f91544c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e208c72f0d7b9952ff5221e6df2cf39a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9479612366912aeae73a",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "ce860b80741c445637cb",
+    "url": "/static/css/main.11daa6d5.chunk.css"
+  },
+  {
+    "revision": "9479612366912aeae73a",
+    "url": "/static/js/2.51d20462.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.51d20462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce860b80741c445637cb",
+    "url": "/static/js/main.a9ec93f7.chunk.js"
+  },
+  {
+    "revision": "b18698950bdb3619231c",
+    "url": "/static/js/runtime-main.8fa8c2fa.js"
+  }
+]);
