fac402f3d207673b8c97f1fc4cdb1947782d2a96
GrafSoul <grafsoul@gmail.com>
2020-10-18 13:50:26 +0500

    feat: Removed the not needed functionality.

--- client/src/TextReader/TextReader.js
+++ client/src/TextReader/TextReader.js
@@ -31,23 +31,9 @@ const TextReader = () => {
 
     const [isPaused, setIsPaused] = useState(true);
     const [isSpeak, setIsSpeak] = useState(true);
-    const [isRecord, setIsRecord] = useState(false);
-    const [isCopied, setIsCopied] = useState(false);
 
     const utterance = new SpeechSynthesisUtterance();
     const voicesAvailable = speechSynthesis.getVoices();
-
-    // eslint-disable-next-line no-undef
-    const recognition = useRef(null);
-
-    useEffect(() => {
-        // eslint-disable-next-line no-undef
-        recognition.current = new webkitSpeechRecognition();
-        recognition.current.continuous = true;
-        recognition.current.interimResults = true;
-
-    },[recognition]);
-
 
     if (window.SpeechSynthesisUtterance === undefined && !voicesAvailable) {
         compatibility.current = false;
@@ -204,55 +190,6 @@ const TextReader = () => {
             panelRef.current.innerHTML += html;
         }
     };
-
-    const handlerToggleRecord = () => {
-        if(!isRecord){
-            setIsRecord(true); 
-            recognition.current.onresult = (event) =>{                 
-                let output = text === '' ? text : text + ' ';
-
-                for(var i=0; i<event.results.length; i++){
-                    output = output + event.results[i][0].transcript ;
-                }
-                
-                setText(output);       
-            }
-            recognition.current.start();
-        } else {
-            setIsRecord(false); 
-            recognition.current.stop();
-        }
-    }
-
-    const handlerCreateEmail = () => {
-        setIsRecord(false); 
-        recognition.current.stop();
-
-        let n = text.indexOf('\n');
-        if (n < 0 || n >= 80) {
-            n = 40 + text.substring(40).indexOf(' ');
-        }
-        let subject = encodeURI(text.substring(0, n));
-        let body = encodeURI(text.substring(n + 1));
-        window.location.href = 'mailto:?subject=' + subject + '&body=' + body;
-    }
-
-    const handleCopyLink = () => {
-        setIsRecord(false); 
-        recognition.current.stop();
-
-        navigator.clipboard
-            .writeText(text)
-            .then(() => {
-                setIsCopied(true);
-                setTimeout(() => {
-                    setIsCopied(false);
-                }, 1000);
-            })
-            .catch((err) => {
-                console.log('Something went wrong', err);
-            });
-    }
 
     return (
         <>
@@ -436,44 +373,6 @@ const TextReader = () => {
                                         Clear
                                     </Button>
                                     </FormGroup>
-
-                                    <FormGroup className={classes.buttonGroup}>
-                                    <Button
-                                        type="button"
-                                        id="button-record"
-                                        color={isRecord ? 'danger': 'success'}
-                                        className={[classes.button, isRecord ? classes.blink: null].join(' ')}
-                                        onClick={handlerToggleRecord}
-                                    >
-                                        <i className="fas fa-microphone-alt"></i>{' '}
-                                        Record
-                                    </Button>
-
-                                    <Button
-                                        disabled={!text ? true : false}
-                                        type="button"
-                                        id="button-record"
-                                        color="info"
-                                        className={classes.button}
-                                        onClick={handlerCreateEmail}
-                                    >
-                                        <i className="fas fa-envelope"></i>{' '}
-                                        Email
-                                    </Button>
-
-                                    <Button
-                                        disabled={!text ? true : false}
-                                        type="button"
-                                        id="button-record"
-                                        color={isCopied ? 'success' : 'info'}
-                                        className={classes.button}
-                                        onClick={handleCopyLink}
-                                        style={{display: 'inline-block', width: '100px'}}
-                                    >
-                                        <i className="fas fa-copy"></i>{' '}
-                                        {isCopied ? 'Copied' : 'Copy'}
-                                    </Button>
-                                    </FormGroup> 
                             </Form>
                         </Jumbotron>                        
                     </div>
